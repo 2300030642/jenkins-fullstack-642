d7b8a6a238814749e894fdb5c9085eea2fd5574d
Nishitha <nishithanishitha3434@gmail.com>
2025-09-12 01:03:41 +0530

    modified

--- LibraryManagementSystem/src/main/java/com/lib/LibraryManagementSystemApplication.java
+++ LibraryManagementSystem/src/main/java/com/lib/LibraryManagementSystemApplication.java
@@ -2,12 +2,19 @@ package com.lib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryManagementSystemApplication {
+public class LibraryManagementSystemApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagementSystemApplication.class, args);
-	}
+    // This method is needed for WAR deployment on external Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LibraryManagementSystemApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSystemApplication.class, args);
+    }
 }
