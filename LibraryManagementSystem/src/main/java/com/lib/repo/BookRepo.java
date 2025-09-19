dbf1c42ad9a2c34348a4168e0803d0233dd43d42
Nishitha <nishithanishitha3434@gmail.com>
2025-09-19 18:01:00 +0530

    updated

--- LibraryManagementSystem/src/main/java/com/lib/repo/BookRepo.java
+++ LibraryManagementSystem/src/main/java/com/lib/repo/BookRepo.java
@@ -1,9 +1,11 @@
 package com.lib.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lib.entity.Book;
 
+@Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
 	
 }
